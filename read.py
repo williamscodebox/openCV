193d6e159db6ddedb12e7a042f1a509b309a8bb2
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-07 17:38:51 -0400

    updated read.py

--- read.py
+++ read.py
@@ -1,12 +1,27 @@
 import cv2 as cv
 
 # Reading Photos
+######################################################
+# img = cv.imread("Photos/cat_large.jpg")
 
-img = cv.imread("Photos/cat_large.jpg")
+# cv.imshow("Cat", img)
 
-cv.imshow("Cat", img)
+# cv.waitKey(0)
 
 # Reading Videos
+######################################################
+# capture = cv.VideoCapture(0) # Use this to open WebCam
+capture = cv.VideoCapture("Videos/dog.mp4")
 
-cv.waitKey(0)
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow("Video", frame)
+
+    if cv.waitKey(20) & 0xFF==ord("d"):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
 
