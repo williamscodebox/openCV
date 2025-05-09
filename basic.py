00e25376f8fce7b47ed131db8d52742114e3b8db
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-09 11:09:24 -0400

    updated basic

--- basic.py
+++ basic.py
@@ -8,28 +8,32 @@ cv.imshow('Cat', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-# # Blur 
-# blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
-# cv.imshow('Blur', blur)
+# Blur 
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 
-# # Edge Cascade
-# canny = cv.Canny(blur, 125, 175)
-# cv.imshow('Canny Edges', canny)
+# Edge Cascade
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
 
-# # Dilating the image
-# dilated = cv.dilate(canny, (7,7), iterations=3)
-# cv.imshow('Dilated', dilated)
+canny2 = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges2', canny2)
 
-# # Eroding
-# eroded = cv.erode(dilated, (7,7), iterations=3)
-# cv.imshow('Eroded', eroded)
 
-# # Resize
-# resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
-# cv.imshow('Resized', resized)
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=3)
+cv.imshow('Dilated', dilated)
 
-# # Cropping
-# cropped = img[50:200, 200:400]
-# cv.imshow('Cropped', cropped)
+# Eroding
+eroded = cv.erode(dilated, (7,7), iterations=3)
+cv.imshow('Eroded', eroded)
+
+# Resize
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
 
 cv.waitKey(0)
