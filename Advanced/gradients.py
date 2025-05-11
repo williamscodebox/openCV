3b99a098d2774e6f0a4aa50f359c820ef33ca78b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-10 21:39:48 -0400

    updated gradients

--- Advanced/gradients.py
+++ Advanced/gradients.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('../Resources/Photos/park.jpg')
+img = cv.imread('../Photos/park.jpg')
 cv.imshow('Park', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
